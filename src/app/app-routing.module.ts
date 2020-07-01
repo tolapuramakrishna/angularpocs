231d7a5918b2d4ace7813575ed1d95107ba82ed2
Tolapu Ramakrishna <tolapuramakrishna@gmail.com>
2020-07-01 09:30:05 +0530

    changes in app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'',loadChildren:() => import( './form-builder/form-builder.module').then(m => m.FormBuilderModule)}
+  {path:'',loadChildren:() => import( './form-builder/form-builder.module').then(m => m.FormBuilderModule)},
+  {path:'testroute',loadChildren:() => import( './form-builder/form-builder.module').then(m => m.FormBuilderModule)}
 ];
 
 @NgModule({
